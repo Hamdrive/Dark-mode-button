bb65760041c00fbe9326642fa563a4c72b848386
Hamza <hamza.husein017@gmail.com>
2021-01-21 13:33:00 +0530

    add theme toggle button

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './styles.css';
+
+function App() {
+
+  const [theme, setTheme] = useState(false)
+
+  return (
+    <div className={theme ? "light-mode" : "dark-mode"}>
+      <main>
+        <h1>Hello {theme ? "Light" : "Dark"} World!</h1>
+        <br />
+      </main>
+
+      <div className="theme-toggle">
+        <button onClick={() => setTheme((prevTheme) => !prevTheme)}>
+          Toggle Theme
+        </button>
+      </div>
+    </div>
+  );
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+  <App />,
+
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
